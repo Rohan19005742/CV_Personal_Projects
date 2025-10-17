58c7740b0f33cdab91c48a78b9edb5ded57867ac
Rohan19005742 <60757115+Rohan19005742@users.noreply.github.com>
2025-10-17 07:37:19 +0100

    optimised counting_sort

--- Sorting_Algorithms/counting_sort.py
+++ Sorting_Algorithms/counting_sort.py
@@ -1,15 +1,17 @@
 data = [0,2,5,3,8,6,4,7,1,9,5,3,2,8,6,4,0,7,1,9]
 
 def counting_sort(arr):
-    count = [0] * 10
-    sorted_data = []
-    for num in arr:
+    max_val = max(arr)
+    count = [0] * (max_val + 1)
+
+    while len(arr) > 0:
+        num = arr.pop(0)
         count[num] += 1
 
     for i in range(len(count)):
-        sorted_data.extend([i] * count[i])
+        arr.extend([i] * count[i])
     
-    return sorted_data
+    return arr
 
 
 if __name__ == "__main__":
